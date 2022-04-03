5cb4d0f0c6846684086d179358f33e1f015c9b68
VIctor <colen.victor@icloud.com>
2022-04-03 19:36:15 -0300

    Initial Commit

---
+++ LP-Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LP-Swift
+//
+//  Created by Victor Colen on 03/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LP-Swift/LP_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LP_SwiftApp.swift
+//  LP-Swift
+//
+//  Created by Victor Colen on 03/04/22.
+//
+
+import SwiftUI
+
+@main
+struct LP_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
